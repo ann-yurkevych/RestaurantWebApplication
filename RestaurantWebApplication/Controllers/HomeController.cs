14eeb356e1945353ea0cb156f3e9860b172b5ccf
ann-yurkevych <97459410+ann-yurkevych@users.noreply.github.com>
2023-11-15 19:41:14 +0000

    Revert "Auth with Google account"
    This reverts commit 499aeba5148945c91accc420e00f050f852f1efb.

--- RestaurantWebApplication/Controllers/HomeController.cs
+++ RestaurantWebApplication/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
 using RestaurantWebApplication.Models;
-using RestSharp;
 using System.Diagnostics;
-using System.Net;
 
 namespace RestaurantWebApplication.Controllers
 {
@@ -17,55 +13,8 @@ namespace RestaurantWebApplication.Controllers
             _logger = logger;
         }
 
-        public ActionResult GoogleLoginCallback(string code)
+        public IActionResult Index()
         {
-            if (code != null)
-            {
-                var client = new RestClient("https://www.googleapis.com/oauth2/v3/token");
-                var request = new RestRequest(Method.POST);
-                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-                request.AddParameter("grant_type", "authorization_code");
-                request.AddParameter("code", code);
-                request.AddParameter("redirect_uri", "https://localhost:7293/Home/GoogleLoginCallback");
-
-                request.AddParameter("client_id", "Enter your clientid here");
-                request.AddParameter("client_secret", "Enter your client secret");
-
-                IRestResponse response = client.Execute(request);
-                var content = response.Content;
-                var res = (JObject)JsonConvert.DeserializeObject(content);
-                var client2 = new RestClient("https://www.googleapis.com/oauth2/v1/userinfo");
-                client2.AddDefaultHeader("Authorization", "Bearer " + res["access_token"]);
-
-                request = new RestRequest(Method.GET);
-
-
-                var response2 = client2.Execute(request);
-
-                var content2 = response2.Content;
-
-                var user = (JObject)JsonConvert.DeserializeObject(content2);
-                return RedirectToAction("Index", "Home", new { loggedIn = true });
-
-            }
-            else
-            {
-                ViewBag.ReturnData = "";
-            }
-
-            return RedirectToAction("Index", "Home");
-        }
-
-        public IActionResult Index(bool? loggedIn)
-        {
-            if (loggedIn == true)
-            {
-                ViewBag.Logged = "Logged In!";
-            }
-            else
-            {
-                ViewBag.Logged = "Not logged In";
-            }
             return View();
         }
 
